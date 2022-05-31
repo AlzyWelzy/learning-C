9704a57295f39b0353a3524f6e96fa82a2e15fee
Alzy Welzy <welzyalzy@gmail.com>
2022-05-31 12:04:25 +0530

    done chapter 5 exercises

--- _06/exercise_04.c
+++ _06/exercise_04.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-void sumAndAverage(int *a, int *b)
+void sumAndAverage(int a, int b, int *sum, float *avg)
 {
-    float i = *a;
-    float j = *b;
-    printf("The sum and average is %0.2f and %0.2f.\n", (i + j), (i + j) / 2);
+    *sum = a + b;
+    *avg = *sum / 2;
 }
 
 int main()
 {
-    int a, b;
+    int a, b, sum;
+    float avg;
     printf("Enter a natural number for a: ");
     scanf("%d", &a);
     printf("Enter a natural number for b: ");
     scanf("%d", &b);
-    sumAndAverage(&a, &b);
+    sumAndAverage(a, b, &sum, &avg);
+    printf("The sum and average of these numbers are %d and %0.2f respectively.", sum, avg);
     return 0;
 }
