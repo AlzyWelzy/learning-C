85cc8b0180741a1c402aded089a849a60d33cba7
Alzy Welzy <welzyalzy@gmail.com>
2022-05-31 12:34:35 +0530

    started chapter 7 [Arrays]

--- _07/arraysBasic.c
+++ _07/arraysBasic.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-    int marks1, marks2, marks3, marks4;
+    // Naive way to create 4 ints
+    // int marks1, marks2, marks3, marks4;
+    // marks1=33;
+    // marks2=34;
+    // marks3=35;
+    // marks4=36;
+
     return 0;
 }
