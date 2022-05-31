ae96cedb8538178a2f5cadc3a3e929d593c78d53
Alzy Welzy <welzyalzy@gmail.com>
2022-05-31 20:44:33 +0530

    doing arrays exercises

--- _07/exercise_03.c
+++ _07/exercise_03.c
@@ -2,18 +2,16 @@
 
 int main()
 {
-    int table[10], num;
-    printf("Enter a number to generate the table of: ");
-    scanf("%d", &num);
+    int table[10];
 
     for (int i = 0; i < 10; i++)
     {
-        table[i] = num * (i + 1);
+        table[i] = 5 * (i + 1);
     }
     printf("%d\n", sizeof(table));
     for (int i = 1; i < 10; i++)
     {
-        printf("The multiple of %d to %d is %d.\n", num, i + 1, table[i]);
+        printf("The multiple of 5 to %d is %d.\n", i + 1, table[i]);
     }
     return 0;
 }
