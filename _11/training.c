1b4d9e661663944c873732c63b645f0b9ad48e01
Alzy Welzy <welzyalzy@gmail.com>
2022-06-04 21:32:19 +0530

    learning Dynamic Memory Allocation

--- _11/training.c
+++ _11/training.c
@@ -41,7 +41,7 @@ int main()
         scanf("%d", &ptr[i]);
     }
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 5; i++)
     {
         printf("The value of %d element is %d\n", i, ptr[i]);
     }
