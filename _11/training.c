b54942aa97c416f1ad6e3ef703493177813652ca
Alzy Welzy <welzyalzy@gmail.com>
2022-06-04 21:36:37 +0530

    Refactored Code

--- _11/training.c
+++ _11/training.c
@@ -29,21 +29,5 @@ int main()
     printf("The size of unsigned long long* is %d\n", sizeof(ptr));
     printf("The size of unsigned short is %d\n", sizeof(unsigned short));
     printf("The size of unsigned short* is %d\n", sizeof(ptr));
-    ptr = malloc(sizeof(int));
-    printf("The size of ptr is %d\n", sizeof(ptr));
-
-    //
-
-    ptr = (int *)malloc(6 * sizeof(int));
-    for (int i = 0; i < 6; i++)
-    {
-        printf("Enter the value of %d element.\n", i);
-        scanf("%d", &ptr[i]);
-    }
-
-    for (int i = 0; i < 6; i++)
-    {
-        printf("The value of %d element is %d\n", i, ptr[i]);
-    }
     return 0;
 }
